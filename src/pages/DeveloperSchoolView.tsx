9597c4f98fb0ec2cac22f9291e90391b19270ce6
ParrePro <parthsdt@gmail.com>
2026-04-02 17:15:16 +0200

    Move TeacherLeaderboard component to developer folder - developer-only feature

--- src/pages/DeveloperSchoolView.tsx
+++ src/pages/DeveloperSchoolView.tsx
@@ -14,7 +14,7 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from 
 import SchoolStats from "@/components/developer/SchoolStats";
 import StudentSearchDelete from "@/components/developer/StudentSearchDelete";
 import PointsTransfer from "@/components/developer/PointsTransfer";
-import TeacherLeaderboard from "@/components/TeacherLeaderboard";
+import TeacherLeaderboard from "@/components/developer/TeacherLeaderboard";
 
 type View = "classes" | "stats" | "students" | "settings" | "transfer";
 
